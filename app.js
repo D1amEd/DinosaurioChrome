fe601d0f2e5d65910bc7275d89175617c69b545e
Edd1e <estebanrco@hotmail.com>
2023-01-31 22:27:56 -0500

    fix conflicts

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+
 const app = document.getElementById("app");
 app.style.width = "100%";
 app.style.height = "100vh";
 app.style.margin = "0";
 app.style.display = "flex";
 console.log(app);
-
+const dino = document.createElement('img')
+dino.setAttribute('src', 'https://www.muycomputer.com/wp-content/uploads/2020/08/dinoswords.gif')
 // Elements
 const canvas = document.createElement("div");
 
@@ -15,7 +17,21 @@ canvas.style.display = "flex";
 canvas.style.alignSelf = "center";
 
 loadElement(app, canvas);
+loadElement(canvas,app)
 
 function loadElement(parent, element) {
   parent.appendChild(element);
 }
+
+
+// let val=0;
+ 
+// dino.addEventListener('keyup',function(e){
+//     if(e.code===arrowUp){
+        
+//     }
+// })
+
+
+
+
