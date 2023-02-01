ea92f7f84439f062c625b38b60a8fc049d2f11c6
Edd1e <estebanrco@hotmail.com>
2023-01-31 23:35:29 -0500

    salto completado

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ app.style.width = "100%";
 app.style.height = "100vh";
 app.style.margin = "0";
 app.style.display = "flex";
-console.log(app);
 const dino = document.createElement('img')
 dino.setAttribute('src', 'https://www.muycomputer.com/wp-content/uploads/2020/08/dinoswords.gif')
+dino.style.position = "absolute"
+
 // Elements
 const canvas = document.createElement("div");
 
@@ -15,22 +16,31 @@ canvas.style.height = "60%";
 canvas.style.backgroundColor = "red";
 canvas.style.display = "flex";
 canvas.style.alignSelf = "center";
+canvas.style.justifyContent = "center"
+canvas.style.alignItems = "center"
 
 loadElement(app, canvas);
-loadElement(canvas,app)
+loadElement(canvas, dino)
 
 function loadElement(parent, element) {
-  parent.appendChild(element);
+    parent.appendChild(element);
 }
+let dinoPos
+document.body.addEventListener('keyup', function (e) {
+    dinoPos = Math.ceil(dino.getBoundingClientRect().top)
+    if (e.code === "ArrowUp") {
+        dinoPos -= 100;
+        dino.style.top = `${dinoPos}px`;
+
+        setTimeout(() => {
+            dinoPos += 100;
+            dino.style.top = `${dinoPos}px`; return dino.style.top
+        }, 100)
+
+    }
+})
 
 
-// let val=0;
- 
-// dino.addEventListener('keyup',function(e){
-//     if(e.code===arrowUp){
-        
-//     }
-// })
 
 
 
